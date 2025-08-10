be82df76c97c7db3718aa8767a72a0159d0bfc41
Asish Kumar Morial <asishmorial@gmail.com>
2025-08-10 17:20:52 +0530

    Implement optimized Valid Anagram check using frequency count

---
+++ Strings/src/ValidAnagram.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter the First String: ");
+            String str1 = sc.nextLine();
+            System.out.print("Enter the Second String: ");
+            String str2 = sc.nextLine();
+            System.out.println(isAnagram(str1,str2) ? "Both the strings are Anagram" : "Both the strings are not Anagram");
+        }
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        int[] frequency = new int[26];
+
+        for (int i = 0; i < str1.length(); i++) {
+            frequency[str1.charAt(i) - 'a']++;
+            frequency[str2.charAt(i) - 'a']--;
+        }
+
+        for (int count : frequency) {
+            if (count != 0)
+                return false;
+        }
+        return true;
+    }
+}
